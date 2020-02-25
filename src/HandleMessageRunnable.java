c76f075859c9becb8703207b86f46c4712f3d5d2
mugdha-gupta <mxg167030@utdallas.edu>
2020-02-24 22:30:25 -0600

    f

--- src/HandleMessageRunnable.java
+++ src/HandleMessageRunnable.java
@@ -1,6 +1,5 @@
 import java.io.IOException;
 
-import static Message.*;
 
 public class HandleMessageRunnable implements Runnable {
     Server server;
@@ -17,7 +16,7 @@ public class HandleMessageRunnable implements Runnable {
 
         switch (message.messageType){
             //implement all switch statemests
-            case APPEND:
+            case Message.APPEND:
                 //can only be coming from the client to the server
                 //need to send to the correct lamportFile
                 try {
@@ -26,7 +25,7 @@ public class HandleMessageRunnable implements Runnable {
                     e.printStackTrace();
                 }
                 break;
-            case ACK:
+            case Message.ACK:
                 if(server.ackMessages.contains(message.fileNum)){
                     message.success = true;
                 }
@@ -35,7 +34,7 @@ public class HandleMessageRunnable implements Runnable {
                 if(message.success){
                     try {
                         server.clients.get(message.clientId).sendMessage(
-                                new Message(ACK, message.fileNum, message.clientId, message.serverId, message.messageNum));
+                                new Message(Message.ACK, message.fileNum, message.clientId, message.serverId, message.messageNum));
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
@@ -47,9 +46,9 @@ public class HandleMessageRunnable implements Runnable {
                     }
                 }
                 break;
-            case RELEASE:
-            case REQUEST:
-            case REPLY:
+            case Message.RELEASE:
+            case Message.REQUEST:
+            case Message.REPLY:
                 //must be coming from another server
                 //need to send to a lamport
                 try {
@@ -58,7 +57,7 @@ public class HandleMessageRunnable implements Runnable {
                     e.printStackTrace();
                 }
                 break;
-            case SERVER_APPEND:
+            case Message.SERVER_APPEND:
                 try {
                     file.writeToFile(message);
                 } catch (IOException e) {
