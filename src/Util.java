7609030a842afcd0426a5986b0e3775e55fda8b9
mugdha-gupta <mxg167030@utdallas.edu>
2020-02-24 22:48:12 -0600

    d

--- src/Util.java
+++ src/Util.java
@@ -11,19 +11,23 @@ public class Util {
      static final int SERVER_LISTENER_PORT = 11888;
      static final int CLIENT_LISTENER_PORT = 11999;
 
+     //returns a socket for the client
     static Socket getSocketAsClient(int serverId, int localPort, int remotePort) throws IOException {
         Socket socket = new Socket();
         socket.setReuseAddress(true);
         InetSocketAddress localInsa = new InetSocketAddress(InetAddress.getLocalHost(), localPort);
         socket.bind(localInsa);
+        System.out.println("bound address");
 
-        InetSocketAddress remoteInsa = new InetSocketAddress(serverAdresses[serverId+1], remotePort);
+        InetSocketAddress remoteInsa = new InetSocketAddress(serverAdresses[serverId], remotePort);
         socket.connect(remoteInsa);
         return socket;
     }
 
+    //returns a socket for the server
     static Socket getSocketAsServer(int remotePort) throws IOException {
         ServerSocket listener = new ServerSocket(remotePort);
+        System.out.println("created listener");
         Socket socket = listener.accept();
         return socket;
     }
