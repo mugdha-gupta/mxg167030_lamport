195972c80d0e335a0959c49d33342ccb47005e7e
mugdha-gupta <mxg167030@utdallas.edu>
2020-02-26 20:16:51 -0600

    d

--- src/MyServerSocket.java
+++ src/MyServerSocket.java
@@ -87,7 +87,7 @@ public class MyServerSocket implements Runnable {
     @Override
     public void run() {
 
-        ExecutorService pool = Executors.newFixedThreadPool(15);
+        ExecutorService pool = Executors.newFixedThreadPool(10);
         Object m;
         while(true){
             try {
